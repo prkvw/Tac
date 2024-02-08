cb4246a4055c48ff2056e379dd75fe54148057e8
prkvw <neeqash@gmail.com>
2024-02-08 16:30:38 +0000

    mapping function update

--- erc777_/src/contract.ts
+++ erc777_/src/contract.ts
@@ -2,11 +2,25 @@ import {
   InterfaceImplementerSet as InterfaceImplementerSetEvent,
   ManagerChanged as ManagerChangedEvent
 } from "../generated/Contract/Contract"
-import { InterfaceImplementerSet, ManagerChanged } from "../generated/schema"
+import { RegisteredToken,InterfaceImplementerSet, ManagerChanged } from "../generated/schema"
 
 export function handleInterfaceImplementerSet(
   event: InterfaceImplementerSetEvent
 ): void {
+  if (event.params.interfaceHash.toHexString() != '0xac7fbab5f54a3ca8194167523c6753bfeb96a445279294b6125b68cce2177054') {
+		return
+	}
+
+	let tokenAddress = event.params.addr
+	let tokenId = tokenAddress.toHexString()
+
+	let token = RegisteredToken.load(tokenId)
+	if (!token) {
+		token = new RegisteredToken(tokenId)
+		token.address = tokenAddress
+		token.save()
+	}
+
   let entity = new InterfaceImplementerSet(
     event.transaction.hash.concatI32(event.logIndex.toI32())
   )
