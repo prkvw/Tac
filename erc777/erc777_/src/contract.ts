481a0afd6e4a6dd48b0cefe7e92454941347dab8
prkvw <neeqash@gmail.com>
2024-02-08 15:05:48 +0000

    Initialize subgraph

---
+++ erc777/erc777_/src/contract.ts
@@ -0,0 +1,36 @@
+import {
+  InterfaceImplementerSet as InterfaceImplementerSetEvent,
+  ManagerChanged as ManagerChangedEvent
+} from "../generated/Contract/Contract"
+import { InterfaceImplementerSet, ManagerChanged } from "../generated/schema"
+
+export function handleInterfaceImplementerSet(
+  event: InterfaceImplementerSetEvent
+): void {
+  let entity = new InterfaceImplementerSet(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.addr = event.params.addr
+  entity.interfaceHash = event.params.interfaceHash
+  entity.implementer = event.params.implementer
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleManagerChanged(event: ManagerChangedEvent): void {
+  let entity = new ManagerChanged(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.addr = event.params.addr
+  entity.newManager = event.params.newManager
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
---
+++ erc777/erc777_/tests/contract-utils.ts
@@ -0,0 +1,57 @@
+import { newMockEvent } from "matchstick-as"
+import { ethereum, Address, Bytes } from "@graphprotocol/graph-ts"
+import {
+  InterfaceImplementerSet,
+  ManagerChanged
+} from "../generated/Contract/Contract"
+
+export function createInterfaceImplementerSetEvent(
+  addr: Address,
+  interfaceHash: Bytes,
+  implementer: Address
+): InterfaceImplementerSet {
+  let interfaceImplementerSetEvent = changetype<InterfaceImplementerSet>(
+    newMockEvent()
+  )
+
+  interfaceImplementerSetEvent.parameters = new Array()
+
+  interfaceImplementerSetEvent.parameters.push(
+    new ethereum.EventParam("addr", ethereum.Value.fromAddress(addr))
+  )
+  interfaceImplementerSetEvent.parameters.push(
+    new ethereum.EventParam(
+      "interfaceHash",
+      ethereum.Value.fromFixedBytes(interfaceHash)
+    )
+  )
+  interfaceImplementerSetEvent.parameters.push(
+    new ethereum.EventParam(
+      "implementer",
+      ethereum.Value.fromAddress(implementer)
+    )
+  )
+
+  return interfaceImplementerSetEvent
+}
+
+export function createManagerChangedEvent(
+  addr: Address,
+  newManager: Address
+): ManagerChanged {
+  let managerChangedEvent = changetype<ManagerChanged>(newMockEvent())
+
+  managerChangedEvent.parameters = new Array()
+
+  managerChangedEvent.parameters.push(
+    new ethereum.EventParam("addr", ethereum.Value.fromAddress(addr))
+  )
+  managerChangedEvent.parameters.push(
+    new ethereum.EventParam(
+      "newManager",
+      ethereum.Value.fromAddress(newManager)
+    )
+  )
+
+  return managerChangedEvent
+}
